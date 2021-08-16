42c638054e955e688fdbf34252aa599a92e40f26
Geroncio de Jesus Rocha Junior <geronciorocha@hotmail.com>
2021-08-16 11:38:43 -0300

    AVALIAÇÃO PRONTA

---
+++ Estoque_A2/src/estoque_a1/Estoque_A1.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estoque_a1;
+
+import FormePrincipal.JFConfianca_estoque;
+
+/**
+ *
+ * @author Cliente
+ */
+public class Estoque_A1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(JFConfianca_estoque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(JFConfianca_estoque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(JFConfianca_estoque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(JFConfianca_estoque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new bd.Bd("");
+                if (bd.Bd.conection.connectionA.config_Serv != null) {
+                    new JFConfianca_estoque().setVisible(true);
+                }
+            }
+        });
+
+    }
+
+}
---
+++ Estoque_A2/verificaExecucao.vbs
@@ -0,0 +1,3 @@
+Dim wshShell
+Set wshShell = CreateObject("WScript.Shell")
+wshShell.Run "X:\Freelance\Estoque2\Estoque_A2\verificaExecucao.bat", 0, false
